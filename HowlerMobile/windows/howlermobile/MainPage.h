4161db7689c5954dac0896ce017cd419f502266a
Cassandra Heart <cassandra.a.heart@gmail.com>
2021-04-04 21:52:16 -0600

    Add Mobile Client base package

---
+++ HowlerMobile/windows/howlermobile/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::howlermobile::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::howlermobile::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
